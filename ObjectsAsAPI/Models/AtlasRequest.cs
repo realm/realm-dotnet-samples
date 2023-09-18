452f248e3e92e030b4bb04b92f0fd46050b7624d
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2023-09-18 09:21:19 +0200

    Stub for new style

--- ObjectsAsAPI/Models/AtlasRequest.cs
+++ ObjectsAsAPI/Models/AtlasRequest.cs
@@ -1,119 +1,32 @@
 ﻿using MongoDB.Bson;
-using ObjectsAsAPI.Services;
-using Realms;
 
 namespace ObjectsAsAPI.Models;
 
-public partial class AtlasRequest : IRealmObject
+public interface IRequest<P, R>
+    where P: IPayload
+    where R: IResponse
 {
-    [PrimaryKey]
-    [MapTo("_id")]
-    public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
+    public ObjectId Id { get; set; }
 
-    [MapTo("_creatorId")]
     public string CreatorId { get; set; }
 
-    [MapTo("status")]
-    private string _Status { get; set; } = null!;
+    public RequestStatus Status { get; set; }
 
-    public RequestStatus Status
-    {
-        get => Enum.Parse<RequestStatus>(_Status);
-        set => _Status = value.ToString();
-    }
+    public P? Payload { get; set; }
 
-    [MapTo("createdAt")]
-    public DateTimeOffset CreatedAt { get; set; }
+    public R? Response { get; set; }
+}
 
-    [MapTo("payload")]
-    public RealmValue Payload { get; set; }
+public interface IPayload
+{
+}
 
-    [MapTo("response")]
-    public RealmValue Response { get; set; }
+public interface IResponse
+{
+    public ResponseStatus Status { get; }
 
-    // Used in the UI
-    public string? Description
-    {
-        get
-        {
-            var payloadClass = Payload.AsIRealmObject().ObjectSchema?.Name;
-
-            string? status = null;
-            string? requestType = null;
-            string? orderIdentifier = null;
-
-            if (payloadClass == nameof(CreateOrderPayload))
-            {
-                requestType = "CreateOrder";
-                orderIdentifier = Payload.AsRealmObject<CreateOrderPayload>()?.Content?.OrderName;
-            }
-            else if(payloadClass == nameof(CancelOrderPayload))
-            {
-                requestType = "CancelOrder";
-                orderIdentifier = Payload.AsRealmObject<CancelOrderPayload>()?.OrderId.ToString();
-            }
-            else
-            {
-                throw new NotImplementedException();
-            }
-
-            if (orderIdentifier?.Length > 10)
-            {
-                orderIdentifier = orderIdentifier[..10];
-            }
-
-            if (Response != RealmValue.Null)
-            {
-                var response = Response.AsIRealmObject() as IResponse;
-
-                status = response?.Status switch
-                {
-                    ResponseStatus.Approved => "✅ ",
-                    ResponseStatus.Rejected => "❌ ",
-                    _ => throw new NotImplementedException(),
-                };
-            }
-
-            return $"{status}{requestType}{(string.IsNullOrEmpty(orderIdentifier)? "" : $" - {orderIdentifier}" )}";
-        }
-    }
-
-    // Used in the UI
-    public string? StatusString
-    {
-        get
-        {
-            var response = Response.AsNullableIRealmObject() as IResponse;
-            return Status switch
-            {
-                RequestStatus.Draft => "Draft",
-                RequestStatus.Pending => "Pending",
-                RequestStatus.Handled => $"{response?.Status}{(string.IsNullOrEmpty(response?.RejectedReason) ? "" : $": \"{response?.RejectedReason}\"")}",
-                _ => throw new NotImplementedException(),
-            };
-        }
-    }
-
-    public AtlasRequest()
-    {
-        if (RealmService.CurrentUser == null)
-        {
-            throw new Exception("Login before using models!");
-        }
-
-        CreatorId = RealmService.CurrentUser.Id;
-        CreatedAt = DateTimeOffset.Now;
-    }
-
-    partial void OnPropertyChanged(string? propertyName)
-    {
-
-        if (propertyName == nameof(_Status))
-        {
-            RaisePropertyChanged(nameof(Status));
-            RaisePropertyChanged(nameof(Description));
-        }
-    }
+    // Filled if Status == Rejected
+    public string? RejectedReason { get; }
 }
 
 public enum RequestStatus
@@ -121,25 +34,6 @@ public enum RequestStatus
     Draft, // The user is still editing, it shouldn't be handled
     Pending, // User confirmed, waiting for backend
     Handled, // Response created
-}
-
-public interface IPayload
-{
-    public string CreatorId { get; set; }
-
-    public ObjectId Id { get; set; }
-}
-
-public interface IResponse
-{
-    public string CreatorId { get; }
-
-    public ObjectId Id { get; }
-
-    public ResponseStatus Status { get; }
-
-    // Filled if Status == Rejected
-    public string? RejectedReason { get; }
 }
 
 public enum ResponseStatus
--- ObjectsAsAPI/Models/CancelOrder.cs
+++ ObjectsAsAPI/Models/CancelOrder.cs
@@ -4,38 +4,14 @@ using Realms;
 
 namespace ObjectsAsAPI.Models;
 
-public partial class CancelOrderPayload : IRealmObject, IPayload
+public partial class CancelOrderPayload : IEmbeddedObject, IPayload
 {
-    [PrimaryKey]
-    [MapTo("_id")]
-    public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
-
-    [MapTo("_creatorId")]
-    public string CreatorId { get; set; }
-
     [MapTo("orderId")]
     public ObjectId OrderId { get; set; }
-
-    public CancelOrderPayload()
-    {
-        if (RealmService.CurrentUser == null)
-        {
-            throw new Exception("Login before using models!");
-        }
-
-        CreatorId = RealmService.CurrentUser.Id;
-    }
 }
 
-public partial class CancelOrderResponse : IRealmObject, IResponse
+public partial class CancelOrderResponse : IEmbeddedObject, IResponse
 {
-    [PrimaryKey]
-    [MapTo("_id")]
-    public ObjectId Id { get; private set; }
-
-    [MapTo("_creatorId")]
-    public string CreatorId { get; private set; } = null!;
-
     [MapTo("orderId")]
     public ObjectId OrderId { get; set; }
 
@@ -44,11 +20,106 @@ public partial class CancelOrderResponse : IRealmObject, IResponse
 
     public ResponseStatus Status
     {
-        //TODO We need to decide if this is the way we want to go
-        get => string.IsNullOrEmpty(_Status)? ResponseStatus.Approved : Enum.Parse<ResponseStatus>(_Status);
+        get => Enum.Parse<ResponseStatus>(_Status);
         private set => _Status = value.ToString();
     }
 
     [MapTo("rejectedReason")]
     public string? RejectedReason { get; private set; }
+}
+
+public partial class CancelOrderRequest : IRealmObject, IRequest<CancelOrderPayload, CancelOrderResponse>
+{
+    [PrimaryKey]
+    [MapTo("_id")]
+    public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
+
+    [MapTo("_creatorId")]
+    public string CreatorId { get; set; }
+
+    [MapTo("status")]
+    private string _Status { get; set; } = null!;
+
+    public RequestStatus Status
+    {
+        get => Enum.Parse<RequestStatus>(_Status);
+        set => _Status = value.ToString();
+    }
+
+    [MapTo("createdAt")]
+    public DateTimeOffset CreatedAt { get; set; }
+
+    [MapTo("payload")]
+    public CancelOrderPayload? Payload { get; set; }
+
+    [MapTo("response")]
+    public CancelOrderResponse? Response { get; set; }
+
+
+    // Used in the UI
+    public string? Description
+    {
+        get
+        {
+            //TODO Check if all correct
+            string? status = null;
+
+            var requestType = "CancelOrder";
+            var orderIdentifier = Payload?.OrderId.ToString();
+
+            if (orderIdentifier?.Length > 10)
+            {
+                orderIdentifier = orderIdentifier[..10];
+            }
+
+            if (Response != RealmValue.Null)
+            {
+                status = Response?.Status switch
+                {
+                    ResponseStatus.Approved => "✅ ",
+                    ResponseStatus.Rejected => "❌ ",
+                    _ => throw new NotImplementedException(),
+                };
+            }
+
+            return $"{status}{requestType}{(string.IsNullOrEmpty(orderIdentifier) ? "" : $" - {orderIdentifier}")}";
+        }
+    }
+
+    // Used in the UI
+    public string? StatusString
+    {
+        get
+        {
+            //TODO CHeck if needed
+            return Status switch
+            {
+                RequestStatus.Draft => "Draft",
+                RequestStatus.Pending => "Pending",
+                RequestStatus.Handled => $"{Response?.Status}{(string.IsNullOrEmpty(Response?.RejectedReason) ? "" : $": \"{Response?.RejectedReason}\"")}",
+                _ => throw new NotImplementedException(),
+            };
+        }
+    }
+
+    public CancelOrderRequest()
+    {
+        if (RealmService.CurrentUser == null)
+        {
+            throw new Exception("Login before using models!");
+        }
+
+        CreatorId = RealmService.CurrentUser.Id;
+        CreatedAt = DateTimeOffset.Now;
+    }
+
+    partial void OnPropertyChanged(string? propertyName)
+    {
+        //TODO Check if needed
+        if (propertyName == nameof(_Status))
+        {
+            RaisePropertyChanged(nameof(Status));
+            RaisePropertyChanged(nameof(Description));
+        }
+    }
 }
