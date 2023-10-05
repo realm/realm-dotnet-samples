992c153b8b15d5d0fa6bf7224a4b37857547d543
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2023-10-05 14:06:25 +0200

    Base app

---
+++ IntelligentCache/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace IntelligentCache;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
+
---
+++ IntelligentCache/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace IntelligentCache;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
